4426a2bfffe94988e02cabf0c7491998568ad965
aikawamoto <ai.vip.jp@gmail.com>
2020-06-22 16:24:09 +0900

    制限時間を付けて、ゲームオーバー、クリア画面を作りました。

--- www/sketch.js
+++ www/sketch.js
@@ -1,5 +1,8 @@
 var y = 600;
 var ex,espeed,ey;
+var rx,rspeed,ry;
+var time=0;
+var lmtm=30;
 var score=0;
 
 function setup(){
@@ -7,33 +10,56 @@ function setup(){
   ex = (random(width));
   espeed = (random(1,6));
   ey = -10;
+
+  rx = (random(width));
+  rspeed = (random(1,6));
+  ry = -10;
   
 }
 function draw(){
   background(255);
   noStroke();
   ell();
+  rec();
+  tm();
   scorep();
   
 }
 
 function ell(){
    fill(255,10,255);
-   rect(mouseX,y,60,20,5);
-   if(dist(mouseX,y,ex,ey)<30){
+   rect(mouseX,y,40,40,5);
+   if(dist(mouseX,y,ex,ey)<40){
     ey =-10;
-    ex = (random(width));  //追加しました
-    espeed = (random(1,6)); //追加しました
+    ex = (random(width));  
+    espeed = (random(1,6)); 
     score+=10;
   }
-   else if(ey-20 > height){
+   else if(ey > height){
     ey =-10;
-    ex = (random(width));  //追加しました
-    espeed = (random(1,6)); //追加しました
+    ex = (random(width));  
+    espeed = (random(1,6)); 
    }
   fill(10,255,10);
-  ellipse(ex,ey,20,20);
+  ellipse(ex,ey,40,40);
   ey += espeed;
+}
+
+  function rec(){
+   if(dist(mouseX,y,rx,ry)<40){
+    ry =-10;
+    rx = (random(width));  
+    rspeed = (random(1,6)); 
+    score-=10;
+  }
+   else if(ry-20 > height){
+    ry =-10;
+    rx = (random(width));  
+    rspeed = (random(1,6)); 
+   }
+  fill(255,255,10);
+  rect(rx,ry,50,50);
+  ry += rspeed;
 
 }
 
@@ -42,5 +68,31 @@ function scorep(){
   fill(200);
   text("score:"+score,10,20);
 
+}
+
+function tm(){
+  time += 1;
+
+if(time%60==0){ //draw()が60で割り切れる回数呼び出された時、if文の中を実行
+
+lmtm -= 1;
+}
+
+fill(20);
+
+textSize(50);
+
+text(nf(lmtm, 2), 100, 50);
+
+if(lmtm<=0 || score<0){
+  background(255,255,0);
+  text("GAME OVER",45,100);
+  noLoop();
+}
+else if(score>40){
+  background(0,255,127);
+  text("GAME CLEAR!!",45,100);
+  noLoop();
+}
 }
 
